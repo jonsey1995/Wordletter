529ce26e889008e3efc0d99321925a730eb7c79a
jonsey1995 <37795811+jonsey1995@users.noreply.github.com>
2018-10-30 13:48:45 +0000

    Add files via upload
    Worksm but doesn't use permeatations

--- Wordladder.c
+++ Wordladder.c
@@ -1,34 +1,64 @@
 #include <stdio.h>
-#include <ctype.h>
-#include <assert.h>
 #include <string.h>
-#define MAXSTRING 100
+#include <assert.h>
+#define MAXCHAR 100
+#define NUMWORDS 2267  //number of 4 letter words in file
+#define STRLENGTH 5
+
 int edit_dist(char *x, char *y);
-int stringlen(char *x);
-int change(char *x, char *y);
-void tests(void);
+void change(char *x, char *y, char fourletterwords[NUMWORDS][STRLENGTH]);
+int wordsarray(char strarr[NUMWORDS][STRLENGTH]);
+void tests(char fourletterwords[NUMWORDS][STRLENGTH]);
 
 int main() {
-   char string1[MAXSTRING], string2[MAXSTRING];
-   int i;
-   printf("enter 2, 4 letter words:\n");
-   scanf("%s", string1);
-   scanf("\n%s", string2);
-   printf("\n");
+  char string1[MAXCHAR], string2[MAXCHAR];
+  int i;
+  /*DICTIONARY*/
+  char fourletterwords[NUMWORDS][STRLENGTH];
+  wordsarray(fourletterwords);
+  tests(fourletterwords);
 
-   tests();
+  /*USER INPUT*/
+  printf("enter 2, 4 letter words:\n");
+  scanf("%s", string1);
+  scanf("\n%s", string2);
+  printf("\n");
 
-   while (edit_dist(string1, string2) != 0) {
-      printf("%s\n",string1);
-      change(string1, string2);
-   }
-   return 0;
+  while (edit_dist(string1, string2) != 0) {
+    change(string1, string2, fourletterwords);
+ }
+
+
+  return 0;
 }
 
+int wordsarray(char strarr[NUMWORDS][STRLENGTH]) {
+  FILE *fpointer;
+  char str[MAXCHAR];  //stores fgets output
+  int i = 0;
+  fpointer = fopen("34words.txt", "r");
+  if (fpointer == NULL) {
+    printf("could not open file\n");
+    return(1);
+  }
+  /*fgets takes pointer to array where read string will be stored, max number of chars to be read,
+  and file stream */
+
+  while (fscanf(fpointer, "%s", str) == 1) {
+    if (strlen(str) == 4) {
+      strcpy(strarr[i], str);
+      /*printf(" strarr[i] =%s str = %s\n", strarr[i], str);*/
+      i++;
+    }
+
+  }
+  fclose(fpointer);
+
+}
 int edit_dist(char *x, char *y){
    int i,count = 0;
    char letter1, letter2;
-   if(stringlen(x) == 4 && stringlen(y)  == 4){
+   if(strlen(x) == 4 && strlen(y)  == 4){
       for (i = 0; i < 4; i++) {
          if (isalpha(*x) && isalpha(*y)) {
             letter1 = tolower(*x);
@@ -51,24 +81,7 @@ int edit_dist(char *x, char *y){
 
    return count;
 }
-int stringlen(char *x){
-   int count = 0;
-   while (*x != '\0') {
-      count++;
-      x++;
-   }
-   return count;
-}
-int change(char *x, char *y){
-   int i;
-   for (i = 0; i < 4; i++) {
-      if (x[i] != y[i]) {
-         x[i] = y[i];
-         printf("%s\n", x);
-      }
-   }
-}
-void tests(void){
+void tests(char fourletterwords[NUMWORDS][STRLENGTH]){
    /*edit_dist testing*/
    assert(edit_dist("chap", "ball") == 4);
    assert(edit_dist("chap", "chat") == 1);
@@ -76,10 +89,38 @@ void tests(void){
    assert(edit_dist("sho^", "shop") == -1);
    assert(edit_dist("shoes", "shoes") == -1);
    assert(edit_dist("shoe", "shoe") == 0);
-   /*stringlen testing*/
-   assert(stringlen("shoe") == 4);
-   assert(stringlen("") == 0);
-   assert(stringlen("  ") == 2);
-   assert(stringlen("shoes") == 5);
-   assert(stringlen("ghfdjsgd") == 8);
+   /*realword testing*/
+   assert(realword(fourletterwords, "work") == 1);
+   assert(realword(fourletterwords, "hgxt") == 0);
+   assert(realword(fourletterwords, "wold") == 1);
+   assert(realword(fourletterwords, "pake") == 0);
+   assert(realword(fourletterwords, "pall") == 1);
+   assert(realword(fourletterwords, "tuns") == 0);
+
+}
+void change(char *x, char *y, char fourletterwords[NUMWORDS][STRLENGTH]){
+   int i;
+   char intermediate[MAXCHAR];
+   strcpy(intermediate, x);  //copy x into intermediate string
+   for (i = 0; i < 4; i++) {
+      if (x[i] != y[i]) {
+         intermediate[i] = y[i];
+         if(realword(fourletterwords, intermediate) == 1){
+            x[i] = intermediate[i];
+            printf("%s\n", x);
+         }
+         else if (realword(fourletterwords, intermediate) == 0) {
+           intermediate[i] = x[i];
+         }
+      }
+   }
+}
+int realword(char fourletterwords[NUMWORDS][STRLENGTH], char test[]) {
+  int i;
+  for (i = 0; i < NUMWORDS; i++) {
+    if (strcmp(test, fourletterwords[i]) == 0) {
+      return 1;
+    }
+  }
+  return 0;
 }
